21e78e019f7888b80fa725dab2ab8d58864ee326
root <cyrenoc@gmail.com>
2020-08-11 04:32:05 +0000

    fixes the list to object json3

--- insertUser.sh
+++ insertUser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo -n apt-get install -y mongodb
+sudo -S apt-get install -y mongodb
 
 echo 'db.createUser({ user: "appAdmin", pwd: "secret",  mechanisms:["SCRAM-SHA-1"] ,roles: [ { role: "clusterAdmin", db: "admin" }, { role: "readAnyDatabase", db: "admin" }, "readWrite" ]});' | mongo  "mongodb://mongoadmin:secret@127.0.0.1:27017/?authSource=admin"
 
