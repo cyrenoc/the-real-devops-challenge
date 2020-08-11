7e18df25c026ccbc03a26b5adf3ab328739a2927
root <cyrenoc@gmail.com>
2020-08-11 15:40:39 +0000

    fixes the list to object json and DB

--- insertUser.sh
+++ insertUser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -y mongodb
+apt-get install -y mongodb
 
 mongoimport --uri "mongodb://appAdmin:secret@127.0.0.1:27017/myNewDatabase"   -c restaurants --file  /home/ubuntu/jenkins/the-real-devops-challenge/data/restaurant.json
 
